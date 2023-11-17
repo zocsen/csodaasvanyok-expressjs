cf889d140acf69c2afd941a9d1c6377fa44208d9
zocsen <zocsen@gmail.com>
2023-11-17 17:22:41 +0100

    Removed totalprice calculations and changed the product to id

--- routes/orders.js
+++ routes/orders.js
@@ -1,202 +1,179 @@
-const { Order } = require("../models/order");
-const express = require("express");
-const { OrderItem } = require("../models/order-item");
-const { Product } = require("../models/product");
+const { Order } = require('../models/order');
+const express = require('express');
+const { OrderItem } = require('../models/order-item');
+const { Product } = require('../models/product');
 const router = express.Router();
 
 const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
-const stripe = require("stripe")(stripeSecretKey);
+const stripe = require('stripe')(stripeSecretKey);
 
 router.get(`/`, async (req, res) => {
-  const orderList = await Order.find()
-    .populate("user", "name")
-    .sort({ dateOrdered: -1 });
+    const orderList = await Order.find().populate('user', 'name').sort({ dateOrdered: -1 });
 
-  if (!orderList) {
-    res.status(500).json({ success: false });
-  }
-  res.send(orderList);
+    if (!orderList) {
+        res.status(500).json({ success: false });
+    }
+    res.send(orderList);
 });
 
 router.get(`/:id`, async (req, res) => {
-  const order = await Order.findById(req.params.id)
-    .populate("user", "name")
-    .populate({
-      path: "orderItems",
-      populate: {
-        path: "product",
-        populate: "category",
-      },
-    });
+    const order = await Order.findById(req.params.id)
+        .populate('user', 'name')
+        .populate({
+            path: 'orderItems',
+            populate: {
+                path: 'product',
+                populate: 'category'
+            }
+        });
 
-  if (!order) {
-    res.status(500).json({ success: false });
-  }
-  res.send(order);
+    if (!order) {
+        res.status(500).json({ success: false });
+    }
+    res.send(order);
 });
 
-router.post("/", async (req, res) => {
-  const orderItemsIds = Promise.all(
-    req.body.orderItems.map(async (orderitem) => {
-      let newOrderItem = new OrderItem({
-        quantity: orderitem.quantity,
-        product: orderitem.product,
-        size: orderitem.size,
-      });
+router.post('/', async (req, res) => {
+    console.log(req.body);
+    const orderItemsIds = Promise.all(
+        req.body.orderItems.map(async (orderitem) => {
+            let newOrderItem = new OrderItem({
+                product: orderitem.id,
+                quantity: orderitem.quantity,
+                size: orderitem.size
+            });
 
-      newOrderItem = await newOrderItem.save();
+            newOrderItem = await newOrderItem.save();
 
-      return newOrderItem._id;
-    })
-  );
+            return newOrderItem._id;
+        })
+    );
 
-  const orderItemsIdsResolved = await orderItemsIds;
+    const orderItemsIdsResolved = await orderItemsIds;
 
-  const totalPrices = await Promise.all(
-    orderItemsIdsResolved.map(async (orderItemId) => {
-      const orderItem = await OrderItem.findById(orderItemId).populate(
-        "product",
-        "price"
-      );
+    let order = new Order({
+        orderItems: orderItemsIdsResolved,
+        shippingAddress1: req.body.shippingAddress1,
+        city: req.body.city,
+        zip: req.body.zip,
+        country: req.body.country,
+        phone: req.body.phone,
+        status: req.body.status,
+        totalPrice: req.body.totalPrice,
+        name: req.body.name,
+        user: req.body.user
+    });
+    order = await order.save();
 
-      const totalPrice = orderItem.product.price * orderItem.quantity;
+    if (!order) return res.status(400).send('the order cannot be created!');
 
-      return totalPrice;
-    })
-  );
-
-  const totalPrice = totalPrices.reduce((a, b) => a + b, 0);
-
-  console.log(totalPrices);
-
-  let order = new Order({
-    orderItems: orderItemsIdsResolved,
-    shippingAddress1: req.body.shippingAddress1,
-    city: req.body.city,
-    zip: req.body.zip,
-    country: req.body.country,
-    phone: req.body.phone,
-    status: req.body.status,
-    totalPrice: totalPrice,
-    name: req.body.name,
-    user: req.body.user,
-  });
-  order = await order.save();
-
-  if (!order) return res.status(400).send("the order cannot be created!");
-
-  res.status(200).send(order);
+    res.status(200).send(order);
 });
 
 router.post('/create-checkout-session', async (req, res) => {
-  const orderItems = req.body.items;
+    const orderItems = req.body.items;
 
-  if (!orderItems) return res.status(400).send('No order items');
+    if (!orderItems) return res.status(400).send('No order items');
 
-  const lineItems = await Promise.all(
-    orderItems.map(async (orderItem) => {
-      const product = await Product.findById(orderItem.productId);
-      const price = product.price;
+    const lineItems = await Promise.all(
+        orderItems.map(async (orderItem) => {
+            const product = await Product.findById(orderItem.productId);
+            const price = product.price;
 
-      const lineItem = {
-        price_data: {
-          currency: 'huf',
-          product_data: {
-            name: product.name,
-            images: [encodeURIComponent(product.image)]
-          },
-          unit_amount: price * 100,
-        },
-        quantity: orderItem.quantity,
-      };
-      return lineItem;
-     }
-  ));
-  const session = await stripe.checkout.sessions.create({
-    payment_method_types: ['card'],
-    line_items: lineItems,
-    mode: 'payment',
-    success_url: 'https://www.csodaasvanyok.hu/success',
-    cancel_url: 'https://www.csodaasvanyok.hu/cancel',
-    locale: 'hu',
-  });
-  res.json({ id: session.id });
-});
-
-
-router.put("/:id", async (req, res) => {
-  const order = await Order.findByIdAndUpdate(
-    req.params.id,
-    {
-      status: req.body.status,
-    },
-    { new: true }
-  );
-
-  if (!order) return res.status(400).send("the order cannot be update!");
-
-  res.send(order);
-});
-
-router.delete("/:id", (req, res) => {
-  Order.findByIdAndRemove(req.params.id)
-    .then(async (order) => {
-      if (order) {
-        await order.orderItems.map(async (orderItem) => {
-          await OrderItem.findByIdAndRemove(orderItem);
-        });
-        return res
-          .status(200)
-          .json({ success: true, message: "the order is deleted!" });
-      } else {
-        return res
-          .status(404)
-          .json({ success: false, message: "order not found!" });
-      }
-    })
-    .catch((err) => {
-      return res.status(500).json({ success: false, error: err });
+            const lineItem = {
+                price_data: {
+                    currency: 'huf',
+                    product_data: {
+                        name: product.name,
+                        images: [encodeURIComponent(product.image)]
+                    },
+                    unit_amount: price * 100
+                },
+                quantity: orderItem.quantity
+            };
+            return lineItem;
+        })
+    );
+    const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items: lineItems,
+        mode: 'payment',
+        success_url: 'https://www.csodaasvanyok.hu/success',
+        cancel_url: 'https://www.csodaasvanyok.hu/cancel',
+        locale: 'hu'
     });
+    res.json({ id: session.id });
 });
 
-router.get("/get/totalsales", async (req, res) => {
-  const totalSales = await Order.aggregate([
-    { $group: { _id: null, totalsales: { $sum: "$totalPrice" } } },
-  ]);
+router.put('/:id', async (req, res) => {
+    const order = await Order.findByIdAndUpdate(
+        req.params.id,
+        {
+            status: req.body.status
+        },
+        { new: true }
+    );
 
-  if (!totalSales) {
-    return res.status(400).send("The order sales cannot be generated");
-  }
+    if (!order) return res.status(400).send('the order cannot be update!');
 
-  res.send({ totalsales: totalSales.pop().totalsales });
+    res.send(order);
+});
+
+router.delete('/:id', (req, res) => {
+    Order.findByIdAndRemove(req.params.id)
+        .then(async (order) => {
+            if (order) {
+                await order.orderItems.map(async (orderItem) => {
+                    await OrderItem.findByIdAndRemove(orderItem);
+                });
+                return res.status(200).json({ success: true, message: 'the order is deleted!' });
+            } else {
+                return res.status(404).json({ success: false, message: 'order not found!' });
+            }
+        })
+        .catch((err) => {
+            return res.status(500).json({ success: false, error: err });
+        });
+});
+
+router.get('/get/totalsales', async (req, res) => {
+    const totalSales = await Order.aggregate([
+        { $group: { _id: null, totalsales: { $sum: '$totalPrice' } } }
+    ]);
+
+    if (!totalSales) {
+        return res.status(400).send('The order sales cannot be generated');
+    }
+
+    res.send({ totalsales: totalSales.pop().totalsales });
 });
 
 router.get(`/get/count`, async (req, res) => {
-  const orderCount = await Order.countDocuments((count) => count);
+    const orderCount = await Order.countDocuments((count) => count);
 
-  if (!orderCount) {
-    res.status(500).json({ success: false });
-  }
-  res.send({
-    orderCount: orderCount,
-  });
+    if (!orderCount) {
+        res.status(500).json({ success: false });
+    }
+    res.send({
+        orderCount: orderCount
+    });
 });
 
 router.get(`/get/userorders/:userid`, async (req, res) => {
-  const userOrderList = await Order.find({ user: req.params.userid })
-    .populate({
-      path: "orderItems",
-      populate: {
-        path: "product",
-        populate: "category",
-      },
-    })
-    .sort({ dateOrdered: -1 });
+    const userOrderList = await Order.find({ user: req.params.userid })
+        .populate({
+            path: 'orderItems',
+            populate: {
+                path: 'product',
+                populate: 'category'
+            }
+        })
+        .sort({ dateOrdered: -1 });
 
-  if (!userOrderList) {
-    res.status(500).json({ success: false });
-  }
-  res.send(userOrderList);
+    if (!userOrderList) {
+        res.status(500).json({ success: false });
+    }
+    res.send(userOrderList);
 });
 
 module.exports = router;
