36a1a3b96c530bc4792cea558902e9c9aea1d3ee
zocsen <zocsen@gmail.com>
2024-03-14 11:08:47 +0100

    mongodb jwt and sharp fix

--- app.js
+++ app.js
@@ -1,49 +1,53 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const morgan = require('morgan');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv/config');
-const authJwt = require('./helpers/jwt');
-const errorHandler = require('./helpers/error-handler');
-const { verifyToken } = require('./authMiddleware');
-const compression = require('compression');
-const path = require('path');
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv/config");
+const authJwt = require("./helpers/jwt");
+const errorHandler = require("./helpers/error-handler");
+const { verifyToken } = require("./authMiddleware");
+const compression = require("compression");
+const path = require("path");
 
 //remove these once stable
 app.use(
-    cors({
-        origin: ['https://www.csodaasvanyok.hu', process.env.LOCALHOST, process.env.ADMINPAGE],
-        optionsSuccessStatus: 200
-        //credentials: true
-    })
+  cors({
+    origin: [
+      "https://www.csodaasvanyok.hu",
+      process.env.LOCALHOST,
+      process.env.ADMINPAGE,
+    ],
+    optionsSuccessStatus: 200,
+    //credentials: true
+  })
 );
 
 // Online payment verification
 app.use(
-    '/.well-known',
-    express.static(path.join(__dirname, '.well-known'), {
-        dotfiles: 'allow'
-    })
+  "/.well-known",
+  express.static(path.join(__dirname, ".well-known"), {
+    dotfiles: "allow",
+  })
 );
 
 //middleware
 app.use(express.json());
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 app.use(authJwt());
-app.use('/public/uploads', express.static(__dirname + '/public/uploads'));
+app.use("/public/uploads", express.static(__dirname + "/public/uploads"));
 app.use(errorHandler);
 app.use(compression());
 
 //Routes
-const categoriesRoutes = require('./routes/categories');
-const mineralsRoutes = require('./routes/minerals');
-const subcategoriesRoutes = require('./routes/subcategories');
-const benefitsRoutes = require('./routes/benefits');
-const colorsRoutes = require('./routes/colors');
-const productsRoutes = require('./routes/products');
-const usersRoutes = require('./routes/users');
-const ordersRoutes = require('./routes/orders');
+const categoriesRoutes = require("./routes/categories");
+const mineralsRoutes = require("./routes/minerals");
+const subcategoriesRoutes = require("./routes/subcategories");
+const benefitsRoutes = require("./routes/benefits");
+const colorsRoutes = require("./routes/colors");
+const productsRoutes = require("./routes/products");
+const usersRoutes = require("./routes/users");
+const ordersRoutes = require("./routes/orders");
 
 const api = process.env.API_URL;
 
@@ -57,26 +61,23 @@ app.use(`${api}/users`, usersRoutes);
 app.use(`${api}/orders`, ordersRoutes);
 
 app.post(`${api}/verifyToken`, verifyToken, (req, res) => {
-    res.json({ isValid: true });
+  res.json({ isValid: true });
 });
 
 //Database
 mongoose
-    .connect(process.env.CONNECTION_STRING, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-        dbName: process.env.DB_NAME
-    })
-    .then(() => {
-        console.log('Database Connection is ready...');
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+  .connect(process.env.CONNECTION_STRING, {
+    dbName: process.env.DB_NAME,
+  })
+  .then(() => {
+    console.log("Database Connection is ready...");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 //Server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server is listening on port ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,34 +1,34 @@
-const expressJwt = require('express-jwt');
+const { expressjwt: expressJwt } = require("express-jwt");
 
 function authJwt() {
-    const secret = process.env.secret;
-    const api = process.env.API_URL;
-    return expressJwt({
-        secret,
-        algorithms: ['HS256'],
-        isRevoked: isRevoked
-    }).unless({
-        path: [
-            { url: /\/public\/uploads(.*)/, methods: ['GET', 'OPTIONS'] },
-            { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'] },
-            { url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },
-            { url: /\/api\/v1\/minerals(.*)/, methods: ['GET', 'OPTIONS'] },
-            { url: /\/api\/v1\/benefits(.*)/, methods: ['GET', 'OPTIONS'] },
-            { url: /\/api\/v1\/subcategories(.*)/, methods: ['GET', 'OPTIONS'] },
-            { url: /\/api\/v1\/colors(.*)/, methods: ['GET', 'OPTIONS'] },
-            { url: /\/api\/v1\/orders(.*)/, methods: ['GET', 'OPTIONS', 'POST'] },
-            `${api}/users/login`,
-            `${api}/users/register`,
-        ]
-    });
+  const secret = process.env.secret;
+  const api = process.env.API_URL;
+  return expressJwt({
+    secret,
+    algorithms: ["HS256"],
+    isRevoked: isRevoked,
+  }).unless({
+    path: [
+      { url: /\/public\/uploads(.*)/, methods: ["GET", "OPTIONS"] },
+      { url: /\/api\/v1\/products(.*)/, methods: ["GET", "OPTIONS"] },
+      { url: /\/api\/v1\/categories(.*)/, methods: ["GET", "OPTIONS"] },
+      { url: /\/api\/v1\/minerals(.*)/, methods: ["GET", "OPTIONS"] },
+      { url: /\/api\/v1\/benefits(.*)/, methods: ["GET", "OPTIONS"] },
+      { url: /\/api\/v1\/subcategories(.*)/, methods: ["GET", "OPTIONS"] },
+      { url: /\/api\/v1\/colors(.*)/, methods: ["GET", "OPTIONS"] },
+      { url: /\/api\/v1\/orders(.*)/, methods: ["GET", "OPTIONS", "POST"] },
+      `${api}/users/login`,
+      `${api}/users/register`,
+    ],
+  });
 }
 
 async function isRevoked(req, payload, done) {
-    if (!payload.isAdmin) {
-        done(null, true);
-    }
+  if (!payload.isAdmin) {
+    done(null, true);
+  }
 
-    done();
+  done();
 }
 
 module.exports = authJwt;
